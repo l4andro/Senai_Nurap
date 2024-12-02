74951441d0cced50772c4ef08c8376e992bd51db
L4andro <101463374+l4andro@users.noreply.github.com>
2024-12-02 10:01:06 -0300

    Add files via upload

---
+++ aula 2/aula02.py
@@ -0,0 +1,8 @@
+print('Bom dia')
+
+nome_usuario = 'leandro'
+idade_usuario = 18
+altura_usuario = 2.01
+maior_de_idade = True #tipo -> bool = boolean
+
+print('Seja bem-vindo!!', nome_usuario, 'sua idade é', idade_usuario, 'sua altura é', altura_usuario, 'voce é maior de idade?', maior_de_idade)
